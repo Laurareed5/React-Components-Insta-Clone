e14a0c68745049f8fe5fbcd57f6f9ef0999bc896
Laura reed <pilesoflaura@gmail.com>
2020-04-25 11:09:24 -0600

    If run, this code will have imported comment section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 import PostsPage from "./components/PostsContainer/PostsPage.js";
+import SearchBar from "./components/SearchBar/SearchBarContainer.js";
+import CommentSection from "./components/CommentSection/CommentSectionContainer.js";
 
 // import the PostsPage and SearchBar and add them to the App
 
@@ -15,7 +17,11 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      
+        <PostsPage />
+      <SearchBar />
+      <CommentSection />
+    
     </div>
   );
 };
